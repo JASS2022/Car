d10e38b50160766af8149336768711f56ce8a0cd
Christoph Weinhuber <christoph.weinhuber@tum.de>
2022-06-15 12:37:16 +0200

    Added april tag detector

--- packages/my_package/src/webconnection.py
+++ packages/my_package/src/webconnection.py
@@ -3,10 +3,25 @@ import websockets
 
 message = '{"type": "locationUpdate","data":{"aprilTag":20}}'
 
+# initial message --> Assumption: We'll always start from 'Hotel'
+# {"type":"initialCarLocation","data":{"trip":[{"x":1,"y":3}]}}
+# follow up
+# {"type": "locationUpdate","data":{"aprilTag":6}}
+
+
 async def hello():
     async with websockets.connect("ws://192.168.8.128:8080") as websocket:
         await websocket.send(message)
         reply = await websocket.recv()
         print(f"Received {reply}")
 
-asyncio.run(hello())
+# asyncio.run(hello())
+
+if __name__ == '__main__':
+    user_input = ""
+    while user_input != "q":
+        user_input = input("Enter command:\n>> ")
+
+        if user_input == "hotel_start":
+            print("Successfully send hotel_start")
+            asy
